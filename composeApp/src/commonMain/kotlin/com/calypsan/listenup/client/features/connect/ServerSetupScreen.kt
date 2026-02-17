b401e4cafac68b3ff108b212863e8ce4022c64d5
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-02-17 11:55:39 -0700

    🐛 Disable autocorrect on server URL field
    Closes #103

--- composeApp/src/commonMain/kotlin/com/calypsan/listenup/client/features/connect/ServerSetupScreen.kt
+++ composeApp/src/commonMain/kotlin/com/calypsan/listenup/client/features/connect/ServerSetupScreen.kt
@@ -245,6 +245,7 @@ private fun FormContent(
             supportingText = state.error?.message,
             keyboardOptions =
                 KeyboardOptions(
+                    autoCorrect = false,
                     keyboardType = KeyboardType.Uri,
                     imeAction = ImeAction.Done,
                 ),
